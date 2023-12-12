1681c4254dd24ed0b9fe5300abd95a11ec8d7742
NeyamulOmy <neyamul.omy@gmail.com>
2023-12-12 15:33:43 +0600

    prblm fixed

--- src/main.js
+++ src/main.js
@@ -1,29 +1,44 @@
-import React, {useEffect, useRef, useState} from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { motion, useInView } from 'framer-motion'
 import NavBar from './navbar';
 
 
 
 function MainIndex() {
-const ref = useRef(null);
-const isInView = useInView(ref);
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+  const [theme, setTheme] = useState(localStorage.getItem('theme'));
 
-useEffect(() => {
 
-console.log("in view 1",isInView);
-}, [isInView])
+  useEffect(() => {
 
-const darkMode = localStorage.getItem('theme') === 'dark';
+    const storedDarkMode = localStorage.getItem('theme')
+    // setTheme(storedDarkMode)
 
-// const textColor = darkMode ? 'text-darkorange' : "text-navlight"
+    if (storedDarkMode === 'dark') {
+      document.documentElement.classList.add('dark')
+    }
+    else {
+      document.documentElement.classList.remove('dark')
+    }
 
-const [textColor, setTextColor] = useState('');
+  }, [theme])
 
-useEffect(() => {
-  
- darkMode ? setTextColor('text-darkorange') : setTextColor("text-navlight")
+  useEffect(() => {
 
-}, [darkMode])
+    // console.log("in view 1", isInView);
+  }, [isInView])
+
+
+  // const textColor = theme ? 'text-darkorange' : "text-navlight"
+
+  const [textColor, setTextColor] = useState('text-navlight');
+
+  useEffect(() => {
+
+    theme === 'dark' ? setTextColor('text-darkorange') : setTextColor("text-navlight")
+
+  }, [theme])
 
 
 
@@ -32,11 +47,11 @@ useEffect(() => {
   return (
     <div ref={ref} className='body  scroll-smooth overflow-x-hidden dark:bg-dark' style={{ transition: 'background-color 1s' }}>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      {isInView && <NavBar textColor={textColor}/>}
-      <motion.div  className=' w-full bg-light h-screen  ' initial={{ opacity:0}}  transition={{duration:1}}></motion.div>
+      {isInView && <NavBar theme={theme} setTheme={setTheme} textColor={textColor} />}
+      <motion.div className=' w-full bg-light h-screen  ' initial={{ opacity: 0 }} transition={{ duration: 1 }}></motion.div>
 
-    
-       </div>
+
+    </div>
   )
 }
 
--- src/navbar.js
+++ src/navbar.js
@@ -1,55 +1,30 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from './button';
 
-function NavBar({ textColor }) {
-
-const [darkmode, setDarkMode] = useState(localStorage.getItem('theme') || 'light');
-
-
-useEffect(() => {
-
-const storedDarkMode = localStorage.getItem('theme') === 'dark'
-// setDarkMode(storedDarkMode)
-
-if(storedDarkMode){
-document.documentElement.classList.add('dark')
-}
-else{
-document.documentElement.classList.remove('dark')
-} 
-
-console.log("darkmode", darkmode);
-},[darkmode])
-  
-const toggleDarkMode = () => {
-const newDarkMode = !darkmode
-setDarkMode(newDarkMode)
-localStorage.setItem('theme', newDarkMode ? 'dark' : 'light')
-
-if(newDarkMode){
-document.documentElement.classList.add('dark')
-}
-else{
-document.documentElement.classList.remove('dark')
-}
+function NavBar({ textColor, theme, setTheme }) {
 
 
 
-}
+  const toggleDarkMode = () => {
+    const newDarkMode = theme === 'light' ? 'dark' : 'light';
+    setTheme(newDarkMode)
+    localStorage.setItem('theme', newDarkMode)
+
+  }
 
 
 
   return (
     <div className={`${textColor} font-lubalin h-[50px] bg-transparent text-[25px] flex flex-row justify-between `} >
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-     <img className='bg-orange-500' alt='logo'></img>
-     <div className='right_components flex flex-row justify-between items-center '>
+      <img className='bg-orange-500' alt='logo'></img>
+      <div className='right_components flex flex-row justify-between items-center '>
 
-<Button text = {"about"} textColor = {textColor} />
-<Button text = {"projects"} textColor = {textColor}/>
-<Button text = {"contact"} textColor = {textColor}/>
-<Button text = {"theme"} textColor = {textColor} onClick={toggleDarkMode}/>
-     </div>
+        <Button text={"about"} textColor={textColor} />
+        <Button text={"projects"} textColor={textColor} />
+        <Button text={"contact"} textColor={textColor} />
+        <Button text={"theme"} textColor={textColor} onClick={toggleDarkMode} />
+      </div>
     </div>
   );
 }
